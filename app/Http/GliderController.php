8ca32a830a6bd6e6baa52b395432451fe33e048c
Michael Rook <michael@rook.net.au>
2022-05-18 12:24:25 +1000

    Update controller response.

--- app/Http/GliderController.php
+++ app/Http/GliderController.php
@@ -16,11 +16,17 @@ class GliderController extends GlideController
      */
     public function generateByPath($path)
     {
+        // Convert :: in url to /
+        $path = Str::replaceFirst('::', '/', $path);
+
+        // Convert first / in url back to :: as we will base the ID from that.
+        $path = Str::replaceFirst('/', '::', $path);
+
         $params = request()->except(['id', 'src', 'path']);
         $params['src'] = $path;
 
         $glideUrl = Statamic::tag('glide')->params($params)->fetch();
 
-        return ['url' => $glideUrl];
+        return redirect($glideUrl, 302);
     }
 }
